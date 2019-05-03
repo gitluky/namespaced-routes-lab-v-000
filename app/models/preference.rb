df56922c8697f36311d240d656c2f52fa6dd8fd8
Jonathan Luk <jluk87@gmail.com>
2019-05-03 19:53:12 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -4,7 +4,7 @@ class Preference < ActiveRecord::Base
     if self.artist_sort_order == "DESC"
       artists.sort_by { |artist| -artist.name }
     else
-      self.artists.sort_by {|artist| artist.name}
+      self.artists.sort_by {|artist| artist.name }
     end
   end
 end
