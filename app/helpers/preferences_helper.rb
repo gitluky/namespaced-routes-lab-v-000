0e4ba457adbd1a35f3943d1638df2869652ea557
Jonathan Luk <jluk87@gmail.com>
2019-05-03 19:57:08 +0000

    Automatically backed up by Learn

--- app/helpers/preferences_helper.rb
+++ app/helpers/preferences_helper.rb
@@ -1,3 +1,17 @@
 module PreferencesHelper
+  def sort_artist(preference, artists)
+    if preference.artist_sort_order == "DESC"
+      artists.sort_by { |artist| -artist.name }
+    else
+      preference.artists.sort_by {|artist| artist.name }
+    end
+  end
 
+  def sort_song(preference, songs)
+    if preference.song_sort_order == "DESC"
+      songs.sort_by { |song| -song.name }
+    else
+      preference.songs.sort_by {|song| song.name }
+    end
+  end
 end
