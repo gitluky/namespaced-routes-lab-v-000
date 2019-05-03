49af9f11a01fe130906af42271de69acf1b349dd
Jonathan Luk <jluk87@gmail.com>
2019-05-03 19:59:07 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,12 @@
 class SongsController < ApplicationController
   def index
+    preference = Preference.last
     if params[:artist_id]
       @artist = Artist.find_by(id: params[:artist_id])
       if @artist.nil?
         redirect_to artists_path, alert: "Artist not found"
       else
-        @songs = @artist.songs
+        @songs = sort_songs(@artist.songs)
       end
     else
       @songs = Song.all
