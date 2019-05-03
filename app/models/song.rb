e02f1904efbeb1c2691158184001e854ba2ac650
Jonathan Luk <jluk87@gmail.com>
2019-05-03 19:43:45 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,9 @@ class Song < ActiveRecord::Base
   end
 
   def artist_name=(name)
-    artist = Artist.find_or_create_by(name: name)
-    self.artist = artist
+    if !params[:artist_name].blank?
+      artist = Artist.find_or_create_by(name: name)
+      self.artist = artist
+    end
   end
 end
