1243eadb488336d6fd80213b0f1e9048bec790eb
Jonathan Luk <jluk87@gmail.com>
2019-05-03 19:56:24 +0000

    Automatically backed up by Learn

--- app/models/preference.rb
+++ app/models/preference.rb
@@ -1,18 +1,10 @@
 class Preference < ActiveRecord::Base
 
-  def sort_artist(artists)
+  def sort_artist(preference, artists)
     if self.artist_sort_order == "DESC"
       artists.sort_by { |artist| -artist.name }
     else
       self.artists.sort_by {|artist| artist.name }
-    end
-  end
-
-  def sort_song(songs)
-    if self.song_sort_order == "DESC"
-      songs.sort_by { |song| -song.name }
-    else
-      self.songs.sort_by {|song| song.name }
     end
   end
 
