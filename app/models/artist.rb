e1508800e7ca2a0c3aae285383f64d69b5557c19
Jonathan Luk <jluk87@gmail.com>
2019-05-03 20:01:39 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,3 +1,12 @@
 class Artist < ActiveRecord::Base
   has_many :songs
+
+  def sort_artists(preference, artists)
+    if preference.artist_sort_order == "DESC"
+      artists.sort_by { |artist| -artist.name }
+    else
+      preference.artists.sort_by {|artist| artist.name }
+    end
+  end
+
 end
